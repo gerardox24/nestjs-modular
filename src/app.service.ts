6a3c2552e005a565713d098625ffca8315bc72b2
Johnny Ramos <johnny.gerardo24@hotmail.com>
2021-08-20 18:38:48 -0500

    adding docker-compose and postgresql connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
 import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     //@Inject('API_KEY') private apiKey: string,
+    @Inject('PG') private clientPG: Client,
     @Inject('TASKS') private tasks: any[],
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
@@ -15,5 +17,14 @@ export class AppService {
     const apiKey = this.configService.apiKey;
     const dbName = this.configService.database.name;
     return `Hello World! ${apiKey} ${dbName}`;
+  }
+
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      this.clientPG.query('SELECT * FROM tasks;', (err, res) => {
+        if (err) reject(err);
+        resolve(res.rows);
+      });
+    });
   }
 }
